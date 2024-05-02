80fcd1eb9e911596eb3ee870a79d4d78da3407e5
oooyyyyyyyy <168528579+oooyyyyyyyy@users.noreply.github.com>
2024-05-02 19:20:14 +0800

    new update

---
+++ root/app.py
@@ -0,0 +1,40 @@
+import os
+
+import requests
+
+from flask import Flask, render_template, url_for, request, jsonify
+
+import datetime
+
+from time import sleep
+
+
+
+
+
+app = Flask(__name__)
+
+
+
+
+@app.route('/')
+
+def index (name=None):
+
+    time = datetime.datetime.now()
+
+    time = time.strftime('%Y %m %d %H:%M:%S')
+
+    return render_template('index.html',name=name,time=time)
+
+
+@app.route('/daima')
+
+def get_yuan():
+
+    return render_template('yuan.html')
+
+if __name__ == '__main__':
+
+    app.run(port=23333,debug=False)
+
